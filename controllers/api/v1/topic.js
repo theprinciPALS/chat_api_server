f216757fde8cc9db4f20089679122aa3cd423409
rzig <railsrocks@users.noreply.github.com>
2018-03-31 13:52:19 -0700

    created show method in api v1 topic controller

--- controllers/api/v1/topic.js
+++ controllers/api/v1/topic.js
@@ -13,5 +13,20 @@ module.exports = {
         resolve(h.response({error: err}).code(500));
       }
     });
+  },
+
+  /**
+   * Returns the topic with the provided ID
+   */
+  show: function(req, h) {
+    return new Promise(async function(resolve, reject){
+      try {
+        topic = await new Topic({id: req.params.id}).fetch();
+        console.log("topic is " + topic);
+        resolve(h.response(JSON.parse(JSON.stringify(topic))).code(302));
+      } catch (err) {
+        resolve(h.response({statusCode: 404, error: "Not Found", message: "Topic not found"}).code(404));
+      }
+    });
   }
 }
