7ad139dfd61a84bded3f044eb7cb6a3e547e34f7
rzig <railsrocks@users.noreply.github.com>
2018-03-31 13:47:30 -0700

    wrote tests for api v1 topic controller get specific device

--- test/controllers/api/v1/topic.test.js
+++ test/controllers/api/v1/topic.test.js
@@ -30,7 +30,7 @@ lab.experiment("GET /topic", () => {
     expect(res.statusCode).to.equal(200);
   });
 
-  lab.test("returns an array of topic", async () => {
+  lab.test("returns an array of topics", async () => {
     sandbox.stub(Topic.prototype, "fetchAll").resolves([new Topic({
       id: 0,
       name: "Ryan Is Awesome",
@@ -61,6 +61,73 @@ lab.experiment("GET /topic", () => {
           underReview: false
         }
       ]
+    });
+  });
+});
+
+lab.experiment("GET /topic/:id", async () => {
+  lab.beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  lab.afterEach(() => {
+    sandbox.restore();
+  });
+
+  lab.test("returns 302 found if the record exists", async () => {
+    sandbox.stub(Topic.prototype, "fetch").resolves(new Topic({
+      id: 0,
+      name: "Ryan Is Awesome",
+      creatorEmail: "abc@example.com",
+      underReview: true
+    });
+    res = await server.inject({
+      url: "/topic/1",
+      method: "GET"
+    });
+    expect(res.statusCode).to.equal(302);
+  });
+
+  lab.test("returns the correct topic if found", async () => {
+    sandbox.stub(Topic.prototype, "fetch").resolves(new Topic({
+      id: 0,
+      name: "Ryan Is Awesome",
+      creatorEmail: "abc@example.com",
+      underReview: true
+    });
+    res = await server.inject({
+      url: "/topic/0",
+      method: "GET"
+    });
+    console.log(JSON.stringify("result is " + res.result));
+    expect(res.result).to.deep.equal({
+      id: 0,
+      name: "Ryan Is Awesome",
+      creatorEmail: "abc@example.com",
+      underReview: true
+    });
+  });
+
+  lab.test("returns 404 not found if the deivce does not exist", async () => {
+    sandbox.stub(Topic.prototype, "fetch").rejects(new Error("Not found"));
+    res = await server.inject({
+      url: "/topic/100",
+      method: "GET"
+    });
+    expect(res.statusCode).to.equal(404);
+  });
+
+  lab.test("returns an error if the topic does not exist", async () => {
+    sandbox.stub(Topic.prototype, "fetch").rejects(new Error("Not found"));
+    res = await server.inject({
+      url: "/topic/100",
+      method: "GET"
+    });
+    // The only important line is the message, but we check everything
+    expect(res.result).to.deep.equal({
+      error: "Not Found",
+      message: "Topic not found",
+      statusCode: 404
     });
   });
 });
