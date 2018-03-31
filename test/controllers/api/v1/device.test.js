49285dd3f0cbab2ccd2e1417e6fd971e5017b00d
rzig <railsrocks@users.noreply.github.com>
2018-03-31 13:34:59 -0700

    created tests for device controller

---
+++ test/controllers/api/v1/device.test.js
@@ -0,0 +1,66 @@
+// Unfortunately, we have to go several directories up :(
+const knex = require("../../../../config/bookshelf").knex;
+// create the tables before we load the server
+knex.migrate.latest().then(() => {
+
+});
+const server = require("../../../../");
+const Lab = require("lab");
+const lab = exports.lab = Lab.script();
+const chai = require("chai");
+const expect = chai.expect;
+const sinon = require("sinon");
+require("sinon-as-promised");
+const Topic = require("../../../../models/topic");
+
+lab.experiment("GET /device", () => {
+  lab.beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  lab.afterEach(() => {
+    sandbox.restore();
+  })
+
+  lab.test("returns a 200 OK", async () => {
+    res = await server.inject({
+      url: "/device",
+      method: "GET"
+    });
+    expect(res.statusCode).to.equal(200);
+  });
+
+  lab.test("returns an array of devices", async () => {
+    sandbox.stub(Topic.prototype, "fetchAll").resolves([new Topic({
+      id: 0,
+      name: "Ryan Is Awesome",
+      creatorEmail: "abc@example.com",
+      underReview: true
+    }), new Topic({
+      id: 1,
+      name: "Joe Is Awesome",
+      creatorEmail: "joe@example.com",
+      underReview: false
+    })]);
+    res = await server.inject({
+      url: "/device",
+      method: "GET"
+    });
+    expect(res.result).to.deep.equal({
+      topics: [
+        {
+          "id": 0,
+          "name": "Ryan is Awesome",
+          "creatorEmail": "abc@example.com",
+          "underReview": true
+        },
+        {
+          "id": 1,
+          "name": "Joe is Awesome",
+          "creatorEmail": "joe@example.com",
+          underReview: false
+        }
+      ]
+    });
+  });
+});
