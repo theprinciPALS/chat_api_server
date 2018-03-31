d16ebf8e550e1684dce357e23231ae10568a0c2a
rzig <railsrocks@users.noreply.github.com>
2018-03-31 13:37:38 -0700

    created index method in api v1 topic controller

---
+++ controllers/api/v1/topic.js
@@ -0,0 +1,18 @@
+const Boom = require("boom");
+const Topic = require("../../../models/topic");
+
+module.exports = {
+  /**
+   * Returns an array of all topics in the database
+   */
+  index: function(req, h) {
+    return new Promise(async function(resolve){
+      try {
+        topics = await Topic.fetchAll();
+        resolve(h.response(JSON.parse(JSON.stringify(topics))).code(200));
+      } catch (err) {
+        resolve(h.response({error: err}).code(500));
+      }
+    });
+  }
+}
