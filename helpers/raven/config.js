4f3ca7dea228dba3cf9f1f14f3c47d88cd5af049
Ryan Ziegler <rzig408@gmail.com>
2018-04-08 11:30:54 -0400

    created raven plugin

---
+++ helpers/raven/config.js
@@ -0,0 +1,11 @@
+var Raven = require("raven");
+exports.plugin = {
+  register: async (server, options) => {
+    Raven.config('https://' + options.dsn + '@sentry.io/' + options.id, {
+      release: options.release
+    }).install();
+    server.Raven = Raven;
+  },
+  name: "raven",
+  version: "0.1.0"
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Hapi=require('hapi');
+const exec = require('child_process').exec;
 
 // Create a server with a host and port
 const server=Hapi.server({
@@ -18,6 +19,21 @@ async function registerRoutes() {
     });
 }
 
+async function registerSentry() {
+  // This will get the SHA1 of out latest release
+  // We provide this to sentry se we can track errors by release
+  await exec('git rev-parse HEAD', async function(err, stdout, stderr) {
+    await server.register({
+      plugin: require("./helpers/raven/config.js"),
+      options: {
+        dsn: process.env.SENTRY_DSN,
+        id: process.env.SENTRY_ID,
+        release: stdout
+      }
+    });
+  });
+}
+
 // Add the route
 server.route({
     method:'GET',
@@ -32,6 +48,7 @@ async function start() {
 
 
   await(registerRoutes());
+  await(registerSentry());
     try {
         await server.start();
     }
