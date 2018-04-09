d8d532663579b01a9586f38a1c8b428b9f0d619e
Ryan Ziegler <rzig408@gmail.com>
2018-04-09 18:15:32 -0400

    moved db to sqlite

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,9 @@
 module.exports = {
 
   development: {
-    client: "postgresql",
+    client: "sqlite3",
     connection: {
-      host: "192.168.2.102",
-      user: "postgres",
-      password: "postgres",
-      database: "chat_api_server_dev",
-      port: 5432
+      filename: "./db/dev.sqlite3"
     },
     migrations: {
       tableName: "knex_migrations"
